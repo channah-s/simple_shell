0216ad9a27b15b19323d65ce01c6d07ffe0e60e9
channah-s <fabohnshannahsado@gmail.com>
2023-07-27 01:05:47 +0100

    updates

--- shell.h
+++ shell.h
@@ -8,20 +8,15 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
 #define MAX_LENGTH 1024
 
 
-extern char **environ;
-
-int execute_command(char *args[], const char *shell_name, int command_count);
-int tokenize_input(char *input, char *args[]);
-void handle_cd(char *args[], const char *shell_name, int command_count);
-int search_n_exec_cmd(char *args[], const char *shell_name, int command_count);
 char *get_shell_name();
-void remove_all_env_except_one(const char *env_var_to_keep);
-void cleanup(char *input);
-void process_input(char *input, const char *shell_name, int *command_count_ptr);
+void handle_cd(char *args[], const char *shell_name, int command_count);
+int tokenize_input(char *input, char *args[]);
+int fork_strtok(char *input);
+int _print(const char *str);
+size_t _strlen(const char *str);
 
 #endif
 
